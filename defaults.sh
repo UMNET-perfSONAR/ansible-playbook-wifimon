6f87c1318f35b3db6297a7a2c49c91d64343d73a
Edward Paul Colone <epcjr@its-perfsonar-bastion.dsc.umich.edu>
2020-07-29 16:06:18 -0400

    improved docs

--- defaults.sh
+++ defaults.sh
@@ -10,14 +10,29 @@ else
 fi
 
 mkdir -p $directory/group_vars/all/
-mkdir -p $directory/group_vars/all/template
-mkdir -p $directory/host_vars/localhost
+mkdir -p $directory/group_vars/all/wifimon
+mkdir -p $directory/host_vars/
 
-# XXX
+# copy hosts file
 wget -q -P $directory -nc \
-  https://raw.githubusercontent.com/UMNET-perfSONAR/ansible-playbook-template/master/inventory/hosts
+  https://raw.githubusercontent.com/UMNET-perfSONAR/ansible-playbook-wifimon/master/inventory/hosts
 
-if ! [ -f $directory/group_vars/all/perfsonar/ps_archive.yml ]; then
-  cp roles/ansible-role-template/defaults/main.yml \
-    $directory/group_vars/all/template/template.yml
+if ! [ -f $directory/group_vars/all/wifimon/elastic.yml ]; then
+  cp roles/ansible-elasticsearch/defaults/main.yml \
+    $directory/group_vars/all/wifimon/elastic.yml
+fi
+
+if ! [ -f $directory/group_vars/all/wifimon/kibana ]; then
+  cp roles/ansible-role-kibana/defaults/main.yml \
+    $directory/group_vars/all/wifimon/kibana.yml
+fi
+
+if ! [ -f $directory/group_vars/all/wifimon/kibana ]; then
+  cp roles/ansible-role-wifimonbuild/defaults/main.yml \
+    $directory/group_vars/all/wifimon/wifimonbuild.yml
+fi
+
+if ! [ -f $directory/group_vars/all/wifimon/kibana ]; then
+  cp roles/ansible-role-wifimon/defaults/main.yml \
+    $directory/group_vars/all/wifimon/wifimon.yml
 fi
